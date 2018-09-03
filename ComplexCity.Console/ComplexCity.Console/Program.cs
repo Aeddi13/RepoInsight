2082ea049ad744b24ebf1bde89f3076b1851988f
Patrick Decker <patrick-andre.decker@centigrade.de>
2018-09-03 10:47:14 +0200

    feat: added solution and project file

---
+++ ComplexCity.Console/ComplexCity.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComplexCity.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
