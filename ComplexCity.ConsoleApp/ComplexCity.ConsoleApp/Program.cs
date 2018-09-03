60e656bf5d03cee1a3dd0013ef403fea8fc7b9d2
Patrick Decker <patrick-andre.decker@centigrade.de>
2018-09-03 11:34:07 +0200

    feat: formatted output

--- ComplexCity.ConsoleApp/ComplexCity.ConsoleApp/Program.cs
+++ ComplexCity.ConsoleApp/ComplexCity.ConsoleApp/Program.cs
@@ -12,9 +12,9 @@ namespace ComplexCity.ConsoleApp
 
             foreach (File file in files)
             {
-                Console.WriteLine(file.FileName);
-                Console.WriteLine("lines of code: " + file.LinesOfCode);
-                Console.WriteLine("number of leading spaces: " + file.NumberOfLeadingSpaces);
+                Console.Write(file.FileName);
+                Console.Write(" | lines of code: " + file.LinesOfCode);
+                Console.WriteLine(" | number of leading spaces: " + file.NumberOfLeadingSpaces);
             }
 
             Console.ReadLine();
