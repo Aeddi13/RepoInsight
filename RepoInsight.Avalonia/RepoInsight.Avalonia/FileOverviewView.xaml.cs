32d516cf1128069d9c75bc754fcaaf282415f3e5
Patrick Decker <patrick.aeddi.decker@googlemail.com>
2018-09-15 15:48:58 +0200

    feat: added first view

---
+++ RepoInsight.Avalonia/RepoInsight.Avalonia/FileOverviewView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RepoInsight.Avalonia
+{
+    public class FileOverviewView : UserControl
+    {
+        public FileOverviewView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
