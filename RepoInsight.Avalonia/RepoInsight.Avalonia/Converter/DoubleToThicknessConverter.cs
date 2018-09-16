3e0f8d8d0e15f701f3280344b3121b13c23b3090
Patrick Decker <patrick.aeddi.decker@googlemail.com>
2018-09-16 17:10:27 +0200

    doc: added documentation for converters

--- RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/DoubleToThicknessConverter.cs
+++ RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/DoubleToThicknessConverter.cs
@@ -5,10 +5,17 @@ using System.Globalization;
 
 namespace RepoInsight.Avalonia.View.Converter
 {
+    /// <summary>
+    /// Converts a double value to a <see cref="Thickness"/> value.
+    /// </summary>
     public class DoubleToThicknessConverter : IValueConverter
     {
+        /// <summary>
+        /// Gets or sets the multiplier for the double value.
+        /// </summary>
         public double Multiplier { get; set; } = 1;
 
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             double doubleValue = System.Convert.ToDouble(value);
@@ -16,6 +23,7 @@ namespace RepoInsight.Avalonia.View.Converter
             return new Thickness(doubleValue * Multiplier);
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
--- RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/MathOperationConverter.cs
+++ RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/MathOperationConverter.cs
@@ -4,23 +4,48 @@ using System.Globalization;
 
 namespace RepoInsight.Avalonia.View.Converter
 {
+    /// <summary>
+    /// Performs a simple math operation on a value that can be converted to <see cref="double"/>.
+    /// </summary>
     public class MathOperationConverter : IValueConverter
     {
-        public string Operator { get; set; }
+        /// <summary>
+        /// Gets or sets the operator for the math operation.
+        /// Allowed values:
+        /// "+" Addition
+        /// "-" Substraction
+        /// "*" Multiplication
+        /// "/" Division
+        /// </summary>
+        public string Operator { get; set; } = "+";
 
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             double doubleValue = System.Convert.ToDouble(value);
             double doubleParameter = System.Convert.ToDouble(parameter);
-
-            if (Operator.Equals("/"))
+            double? returnValue = null;
+            
+            switch (Operator)
             {
-                return doubleValue / doubleParameter;
+                case "+":
+                    returnValue = doubleValue + doubleParameter;
+                    break;
+                case "-":
+                    returnValue = doubleValue - doubleParameter;
+                    break;
+                case "*":
+                    returnValue = doubleValue * doubleParameter;
+                    break;
+                case "/":
+                    returnValue = doubleValue / doubleParameter;
+                    break;
             }
-
-            return null;
+            
+            return returnValue;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
