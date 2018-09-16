35a1021293f96e8646a7f09d6ff79a088600332d
Patrick Decker <patrick.aeddi.decker@googlemail.com>
2018-09-16 10:08:58 +0200

    feat: added multiplier property to DoubleToThicknessConverter

--- RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/DoubleToThicknessConverter.cs
+++ RepoInsight.Avalonia/RepoInsight.Avalonia/Converter/DoubleToThicknessConverter.cs
@@ -7,11 +7,13 @@ namespace RepoInsight.Avalonia.View.Converter
 {
     public class DoubleToThicknessConverter : IValueConverter
     {
+        public double Multiplier { get; set; } = 1;
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             double doubleValue = System.Convert.ToDouble(value);
 
-            return new Thickness(doubleValue);
+            return new Thickness(doubleValue * Multiplier);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
