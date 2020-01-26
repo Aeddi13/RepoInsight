245b0b9bf85182752eb08afa880be6f9d85a1850
Patrick Andre Decker <patrick.aeddi.decker@googlemail.com>
2020-01-26 14:48:33 +0100

    feat: created FileSystemRepoObjectInfoFactory

---
+++ RepoInsight.BusinessLogic/Repository/FileSystemImpl/FileSystemRepoObjectInfoFactory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RepoInsight.BusinessLogic.Repository.FileSystemImpl
+{
+    public class FileSystemRepoObjectInfoFactory : IRepoObjectInfoFactory
+    {
+        /// <summary>
+        /// Creates a list of <see cref="IRepoObjectInfo"/> for the given repository.
+        /// </summary>
+        /// <param name="repositoryPath">
+        /// The path of the repository on the file system.
+        /// </param>
+        /// <returns>A list of <see cref="IRepoObjectInfo"/>.</returns>
+        public List<IRepoObjectInfo> CreateObjectInfos(string repositoryPath)
+        {
+            List<IRepoObjectInfo> objectInfos = new List<IRepoObjectInfo>();
+
+            // add the fileInfos from the subdirectories
+            string[] subdirectories = Directory.GetDirectories(repositoryPath);
+            foreach (string directory in subdirectories)
+            {
+                FolderInfo folderInfo = CreateFolderInfo(directory);
+                objectInfos.Add(folderInfo);
+            }
+
+            // Put all file names in the current directory into an array.
+            string[] fileNames = Directory.GetFiles(repositoryPath);
+            foreach (string fileName in fileNames)
+            {
+                FileInfo fileInfo = CreateFileInfo(fileName);
+
+                objectInfos.Add(fileInfo);
+            }
+
+            return objectInfos;
+        }
+
+        private static FileInfo CreateFileInfo(string fileName)
+        {
+            string fileContent = File.ReadAllText(fileName);
+
+            string[] lines = fileContent.Split('\n');
+
+            int lineCount = lines.Length;
+            int leadingWhitespaceCount = 0;
+
+            foreach (string line in lines)
+            {
+                // skip if line is a comment
+                if (StringHelper.IsLineAComment(line))
+                {
+                    continue;
+                }
+
+                // replace tabs with 4 whitespaces
+                string lineValue = StringHelper.ReplaceTabsWithWhitespaces(line);
+
+                int leadingSpaces = lineValue.TakeWhile(Char.IsWhiteSpace).Count();
+                leadingWhitespaceCount += leadingSpaces;
+            }
+
+            FileInfo fileInfo = new FileInfo()
+            {
+                LinesOfCode = lineCount,
+                Name = fileName,
+                NumberOfLeadingSpaces = leadingWhitespaceCount
+            };
+
+            return fileInfo;
+        }
+
+        private static FolderInfo CreateFolderInfo(string folderName)
+        {
+
+        }
+    }
+}
