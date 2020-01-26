54911d7fbeee91cab7ebbcc28757d600279a483d
Patrick Andre Decker <patrick.aeddi.decker@googlemail.com>
2020-01-26 10:40:35 +0100

    feat: added IRepoObjectInfo interface

---
+++ RepoInsight.BusinessLogic/Repository/IRepoObjectInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoInsight.BusinessLogic.Repository
+{
+    /// <summary>
+    /// Contains information for an object inside a repository.
+    /// This can either be information about a folder or a file.
+    /// </summary>
+    public interface IRepoObjectInfo
+    {
+        string Name
+        {
+            get;
+        }
+
+        int LinesOfCode
+        {
+            get;
+        }
+
+        int NumberOfRevisions
+        {
+            get;
+        }
+    }
+}
